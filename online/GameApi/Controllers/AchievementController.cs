d13c469f74808ccacdf8a50f3f43f3d293141c1e
Hugo Girard <127935820+Hugo0207@users.noreply.github.com>
2025-04-23 14:43:47 -0400

    Feat : achievements 2

---
+++ online/GameApi/Controllers/AchievementController.cs
@@ -0,0 +1,27 @@
+﻿using GameApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameApi.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class AchievementController : ControllerBase
+    {
+        private readonly AchievementService _achievementService;
+
+        public AchievementController(AchievementService achievementService)
+        {
+            _achievementService = achievementService;
+        }
+
+        [HttpGet("details")]
+        public async Task<IActionResult> GetDetailedAchievements()
+        {
+            var username = User.Identity?.Name!;
+            var achievements = await _achievementService.GetDetailedPlayerAchievementsAsync(username);
+            return achievements == null ? NotFound() : Ok(achievements);
+        }
+    }
+}
